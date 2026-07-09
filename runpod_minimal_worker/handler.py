8c8c0360b35fcb1e239675a0b4f4fba378162fca
Robert Sasiak <robert.sasiak@example.com>
2026-07-09 14:43:16 +0100

    Add ping healthcheck for Runpod

--- runpod_minimal_worker/handler.py
+++ runpod_minimal_worker/handler.py
@@ -1,4 +1,13 @@
 import runpod
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/ping")
+def ping():
+    return JSONResponse({"ok": True})
 
 
 def handler(job):
